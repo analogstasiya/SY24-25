b721bd73d8f29bd7137dd15cba667aea7ae58b21
ana <ajfli321@learner.dvusd.org>
2024-09-23 12:52:19 -0700

    added calculator to github

---
+++ Calculator/Calculator/Form1.Designer.vb
@@ -0,0 +1,438 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Display = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        ButtonDecimal = New Button()
+        Button0 = New Button()
+        ButtonEqual = New Button()
+        ButtonPlus = New Button()
+        Button11 = New Button()
+        ButtonTimes = New Button()
+        ButtonDivison = New Button()
+        ButtonDelete = New Button()
+        Button10 = New Button()
+        ButtonMPlus = New Button()
+        ButtonMR = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        SuspendLayout()
+        ' 
+        ' Display
+        ' 
+        Display.BackColor = Color.LavenderBlush
+        Display.Font = New Font("Papyrus", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Display.Location = New Point(0, 0)
+        Display.Margin = New Padding(4, 0, 4, 0)
+        Display.Name = "Display"
+        Display.Size = New Size(999, 79)
+        Display.TabIndex = 0
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.PaleVioletRed
+        Button1.FlatStyle = FlatStyle.Popup
+        Button1.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(0, 76)
+        Button1.Margin = New Padding(4)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(102, 64)
+        Button1.TabIndex = 1
+        Button1.Text = "1"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.PaleVioletRed
+        Button2.FlatStyle = FlatStyle.Popup
+        Button2.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button2.Location = New Point(97, 77)
+        Button2.Margin = New Padding(4)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(102, 64)
+        Button2.TabIndex = 2
+        Button2.Text = "2"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.PaleVioletRed
+        Button3.FlatStyle = FlatStyle.Popup
+        Button3.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button3.Location = New Point(197, 77)
+        Button3.Margin = New Padding(4)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(102, 64)
+        Button3.TabIndex = 3
+        Button3.Text = "3"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.PaleVioletRed
+        Button4.FlatStyle = FlatStyle.Popup
+        Button4.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button4.Location = New Point(0, 137)
+        Button4.Margin = New Padding(4)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(102, 64)
+        Button4.TabIndex = 4
+        Button4.Text = "4"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.PaleVioletRed
+        Button5.FlatStyle = FlatStyle.Popup
+        Button5.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button5.Location = New Point(97, 137)
+        Button5.Margin = New Padding(4)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(102, 64)
+        Button5.TabIndex = 5
+        Button5.Text = "5"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.PaleVioletRed
+        Button6.FlatStyle = FlatStyle.Popup
+        Button6.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button6.Location = New Point(197, 137)
+        Button6.Margin = New Padding(4)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(102, 64)
+        Button6.TabIndex = 6
+        Button6.Text = "6"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.PaleVioletRed
+        Button7.FlatStyle = FlatStyle.Popup
+        Button7.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button7.Location = New Point(0, 199)
+        Button7.Margin = New Padding(4)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(102, 64)
+        Button7.TabIndex = 7
+        Button7.Text = "7"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.PaleVioletRed
+        Button8.FlatStyle = FlatStyle.Popup
+        Button8.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button8.Location = New Point(97, 199)
+        Button8.Margin = New Padding(4)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(102, 64)
+        Button8.TabIndex = 8
+        Button8.Text = "8"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.PaleVioletRed
+        Button9.FlatStyle = FlatStyle.Popup
+        Button9.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button9.Location = New Point(197, 199)
+        Button9.Margin = New Padding(4)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(102, 64)
+        Button9.TabIndex = 9
+        Button9.Text = "9"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonDecimal
+        ' 
+        ButtonDecimal.BackColor = Color.PaleVioletRed
+        ButtonDecimal.FlatStyle = FlatStyle.Popup
+        ButtonDecimal.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonDecimal.Location = New Point(97, 262)
+        ButtonDecimal.Margin = New Padding(4)
+        ButtonDecimal.Name = "ButtonDecimal"
+        ButtonDecimal.Size = New Size(102, 64)
+        ButtonDecimal.TabIndex = 10
+        ButtonDecimal.Text = "."
+        ButtonDecimal.UseVisualStyleBackColor = False
+        ' 
+        ' Button0
+        ' 
+        Button0.BackColor = Color.PaleVioletRed
+        Button0.FlatStyle = FlatStyle.Popup
+        Button0.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button0.Location = New Point(0, 262)
+        Button0.Margin = New Padding(4)
+        Button0.Name = "Button0"
+        Button0.Size = New Size(102, 64)
+        Button0.TabIndex = 11
+        Button0.Text = "0"
+        Button0.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonEqual
+        ' 
+        ButtonEqual.BackColor = Color.PaleVioletRed
+        ButtonEqual.FlatStyle = FlatStyle.Popup
+        ButtonEqual.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonEqual.Location = New Point(197, 262)
+        ButtonEqual.Margin = New Padding(4)
+        ButtonEqual.Name = "ButtonEqual"
+        ButtonEqual.Size = New Size(102, 64)
+        ButtonEqual.TabIndex = 12
+        ButtonEqual.Text = "="
+        ButtonEqual.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonPlus
+        ' 
+        ButtonPlus.BackColor = Color.PaleVioletRed
+        ButtonPlus.FlatStyle = FlatStyle.Popup
+        ButtonPlus.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonPlus.Location = New Point(295, 76)
+        ButtonPlus.Margin = New Padding(4)
+        ButtonPlus.Name = "ButtonPlus"
+        ButtonPlus.Size = New Size(92, 65)
+        ButtonPlus.TabIndex = 13
+        ButtonPlus.Text = "+"
+        ButtonPlus.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.PaleVioletRed
+        Button11.FlatStyle = FlatStyle.Popup
+        Button11.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button11.Location = New Point(295, 137)
+        Button11.Margin = New Padding(4)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(92, 64)
+        Button11.TabIndex = 14
+        Button11.Text = "-"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonTimes
+        ' 
+        ButtonTimes.BackColor = Color.PaleVioletRed
+        ButtonTimes.FlatStyle = FlatStyle.Popup
+        ButtonTimes.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonTimes.Location = New Point(295, 199)
+        ButtonTimes.Margin = New Padding(4)
+        ButtonTimes.Name = "ButtonTimes"
+        ButtonTimes.Size = New Size(92, 64)
+        ButtonTimes.TabIndex = 15
+        ButtonTimes.Text = "x"
+        ButtonTimes.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonDivison
+        ' 
+        ButtonDivison.BackColor = Color.PaleVioletRed
+        ButtonDivison.FlatStyle = FlatStyle.Popup
+        ButtonDivison.Font = New Font("Papyrus", 28.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonDivison.Location = New Point(295, 261)
+        ButtonDivison.Margin = New Padding(4)
+        ButtonDivison.Name = "ButtonDivison"
+        ButtonDivison.Size = New Size(92, 66)
+        ButtonDivison.TabIndex = 16
+        ButtonDivison.Text = "÷"
+        ButtonDivison.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonDelete
+        ' 
+        ButtonDelete.BackColor = Color.Chartreuse
+        ButtonDelete.FlatStyle = FlatStyle.Popup
+        ButtonDelete.Font = New Font("Arial", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        ButtonDelete.Location = New Point(476, 261)
+        ButtonDelete.Margin = New Padding(4)
+        ButtonDelete.Name = "ButtonDelete"
+        ButtonDelete.Size = New Size(70, 64)
+        ButtonDelete.TabIndex = 17
+        ButtonDelete.Text = "cos"
+        ButtonDelete.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.Chartreuse
+        Button10.FlatStyle = FlatStyle.Popup
+        Button10.Font = New Font("Arial Narrow", 22.2F)
+        Button10.Location = New Point(385, 77)
+        Button10.Margin = New Padding(4)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(93, 64)
+        Button10.TabIndex = 18
+        Button10.Text = "AC"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonMPlus
+        ' 
+        ButtonMPlus.BackColor = Color.Chartreuse
+        ButtonMPlus.FlatStyle = FlatStyle.Popup
+        ButtonMPlus.Font = New Font("Arial Narrow", 22.2F)
+        ButtonMPlus.Location = New Point(385, 199)
+        ButtonMPlus.Margin = New Padding(4)
+        ButtonMPlus.Name = "ButtonMPlus"
+        ButtonMPlus.Size = New Size(93, 64)
+        ButtonMPlus.TabIndex = 19
+        ButtonMPlus.Text = "M+"
+        ButtonMPlus.UseVisualStyleBackColor = False
+        ' 
+        ' ButtonMR
+        ' 
+        ButtonMR.BackColor = Color.Chartreuse
+        ButtonMR.FlatStyle = FlatStyle.Popup
+        ButtonMR.Font = New Font("Arial Narrow", 22.2F)
+        ButtonMR.Location = New Point(385, 258)
+        ButtonMR.Margin = New Padding(4)
+        ButtonMR.Name = "ButtonMR"
+        ButtonMR.Size = New Size(93, 67)
+        ButtonMR.TabIndex = 20
+        ButtonMR.Text = "MR"
+        ButtonMR.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.Chartreuse
+        Button12.FlatStyle = FlatStyle.Popup
+        Button12.Font = New Font("Arial Narrow", 22.2F)
+        Button12.Location = New Point(385, 137)
+        Button12.Margin = New Padding(4)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(93, 64)
+        Button12.TabIndex = 21
+        Button12.Text = "x²"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.Chartreuse
+        Button13.FlatStyle = FlatStyle.Popup
+        Button13.Font = New Font("Arial Narrow", 22.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button13.Location = New Point(476, 137)
+        Button13.Margin = New Padding(4)
+        Button13.Name = "Button13"
+        Button13.Size = New Size(70, 67)
+        Button13.TabIndex = 22
+        Button13.Text = "sin"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.Chartreuse
+        Button14.FlatStyle = FlatStyle.Popup
+        Button14.Font = New Font("Arial Narrow", 22.2F)
+        Button14.Location = New Point(476, 77)
+        Button14.Margin = New Padding(4)
+        Button14.Name = "Button14"
+        Button14.Size = New Size(70, 64)
+        Button14.TabIndex = 23
+        Button14.Text = "π"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = Color.Chartreuse
+        Button15.FlatStyle = FlatStyle.Popup
+        Button15.Font = New Font("Arial Narrow", 22.2F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Button15.Location = New Point(476, 199)
+        Button15.Margin = New Padding(4)
+        Button15.Name = "Button15"
+        Button15.Size = New Size(70, 64)
+        Button15.TabIndex = 24
+        Button15.Text = "tan"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 24F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.DarkMagenta
+        ClientSize = New Size(544, 323)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(ButtonMR)
+        Controls.Add(ButtonMPlus)
+        Controls.Add(Button10)
+        Controls.Add(ButtonDelete)
+        Controls.Add(ButtonDivison)
+        Controls.Add(ButtonTimes)
+        Controls.Add(Button11)
+        Controls.Add(ButtonPlus)
+        Controls.Add(ButtonEqual)
+        Controls.Add(Button0)
+        Controls.Add(ButtonDecimal)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Display)
+        Font = New Font("Papyrus", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Margin = New Padding(4)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Display As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents ButtonDecimal As Button
+    Friend WithEvents Button0 As Button
+    Friend WithEvents ButtonEqual As Button
+    Friend WithEvents ButtonPlus As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents ButtonTimes As Button
+    Friend WithEvents ButtonDivison As Button
+    Friend WithEvents ButtonDelete As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents ButtonMPlus As Button
+    Friend WithEvents ButtonMR As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+
+End Class
---
+++ Calculator/Calculator/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub one_Click(sender As Object, e As EventArgs) Handles Button1.Click, Button2.Click, Button3.Click, Button6.Click, Button5.Click, Button4.Click, Button7.Click, Button8.Click, Button9.Click, Button0.Click, ButtonDecimal.Click
+        Display.Text = Display.Text + sender.text
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Display.Text = ""
+    End Sub
+    Dim Mem As String
+    Dim firstnum As Long
+    Dim oper As String
+    Private Sub ButtonMPlus_Click(sender As Object, e As EventArgs) Handles ButtonMPlus.Click
+        Mem = Display.Text
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles ButtonMR.Click
+        Display.Text = Mem
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Dim num As Long
+        num = Display.Text
+        Display.Text = num * num
+    End Sub
+
+    Private Sub ButtonPlus_Click(sender As Object, e As EventArgs) Handles ButtonPlus.Click, Button11.Click, ButtonTimes.Click, ButtonDivison.Click
+        oper = sender.Text '+
+        firstnum = Display.Text
+        Display.Text = ""
+    End Sub
+
+    Private Sub ButtonEqual_Click(sender As Object, e As EventArgs) Handles ButtonEqual.Click
+        If oper = "+" Then
+            Display.Text = firstnum + Display.Text
+        End If
+        If oper = "-" Then
+            Display.Text = firstnum - Display.Text
+        End If
+        If oper = "x" Then
+            Display.Text = firstnum * Display.Text
+        End If
+        If oper = "÷" Then
+            Display.Text = firstnum / Display.Text
+        End If
+    End Sub
+
+    Private Sub Button13_Click_1(sender As Object, e As EventArgs) Handles Button13.Click
+        Display.Text = Math.Sin(Display.Text)
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        Display.Text = Math.PI
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Display.Text = Math.Tan(Display.Text)
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+        Display.Text = Math.Cos(Display.Text)
+    End Sub
+End Class
