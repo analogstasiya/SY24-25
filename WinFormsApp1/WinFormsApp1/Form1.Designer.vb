128ff2010720bc8822f82c6f562886a658b1e388
ana <ajfli321@learner.dvusd.org>
2025-01-16 13:05:23 -0700

    maretu song lyric

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(132, 119)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(387, 94)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(132, 253)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(387, 94)
+        TextBox2.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(580, 203)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(162, 189)
+        Button1.TabIndex = 2
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged, TextBox2.TextChanged
+        If TextBox1.Text Like TextBox2.Text Then
+            Me.BackColor = Color.Green
+        Else
+            Me.BackColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sp As Integer
+        sp = TextBox1.Text.IndexOf(" ")
+        TextBox2.Text = TextBox1.Text.Substring(0, sp)
+    End Sub
+End Class
+
